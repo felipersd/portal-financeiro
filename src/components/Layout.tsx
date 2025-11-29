283327e6b345d2715fd4983be26fa03056adfd13
Felipe <51270679+felipersd@users.noreply.github.com>
2025-11-29 19:53:10 -0300

    Update category label in Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -78,7 +78,7 @@ export const Layout: React.FC = () => {
                         onClick={() => setCurrentView('categories')}
                         className={`nav-item ${currentView === 'categories' ? 'active' : ''}`}
                     >
-                        <Tag size={20} /> Categorias - SELOROOOO
+                        <Tag size={20} /> Categorias - FUNFOOOOOU
                     </li>
                 </ul>
 
