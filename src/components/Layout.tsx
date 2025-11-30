809c04c2d57944af7921cdab0c1e2cb0960b76f1
Felipe <51270679+felipersd@users.noreply.github.com>
2025-11-30 04:19:50 -0300

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -78,7 +78,7 @@ export const Layout: React.FC = () => {
                         onClick={() => setCurrentView('categories')}
                         className={`nav-item ${currentView === 'categories' ? 'active' : ''}`}
                     >
-                        <Tag size={20} /> Categorias -aaaaa
+                        <Tag size={20} /> Categorias
                     </li>
                 </ul>
 
